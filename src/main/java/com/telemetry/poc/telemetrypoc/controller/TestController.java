1d872a51ca95648a17f3877153658bb3b6aec4ae
Raghu Ram Yatam <raghuramyatam@Raghus-MacBook-Pro.local>
2024-07-26 16:42:11 +0530

    added spring boot telemetry part

--- src/main/java/com/telemetry/poc/telemetrypoc/controller/TestController.java
+++ src/main/java/com/telemetry/poc/telemetrypoc/controller/TestController.java
@@ -4,8 +4,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.telemetry.poc.telemetrypoc.bean.Employee;
 
 @RestController
 public class TestController {
@@ -27,6 +31,28 @@ public class TestController {
 	public String another() {
 		logger.info("Another service called");
 		return "Hello from another service!";
+	}
+
+	@PostMapping("/testSampleData")
+	public boolean sampleTest(@RequestBody Employee employee) {
+		logger.info("Employee Details Test Service");
+
+		boolean employeeVerified = detailsCheck(employee);
+
+		logger.info("Received response: {}", employeeVerified);
+		if (employeeVerified == true) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean detailsCheck(Employee employeeDetails) {
+		if (employeeDetails.getName().equalsIgnoreCase("raghu")) {
+			if (employeeDetails.getId() == 7) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
